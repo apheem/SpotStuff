30366b63398fd7aee2ce88844f7b42eaef3bc4e1
apheem <cody12carlson@gmail.com>
2021-04-10 15:22:44 -0400

    Create a way to extract artist and genre

--- spot_api.py
+++ spot_api.py
@@ -18,6 +18,10 @@ for ids in artist_id_array:
     ids = re.sub(r"\]","",ids)
     clean_artists.append(ids)
 
+
+for i in range(len(clean_artists)):
+    clean_artists[i] = clean_artists[i][:22]
+
 import base64
 import requests
 import datetime
@@ -26,96 +30,99 @@ from urllib.parse import urlencode
 client_id = "f8ba7e016c3548a48aad449ab1ba11aa"
 client_secret = "2fabafc2dcef4001a2b4b1cc53b0cf4c"
 
-class SpotifyAPI(object):
-    access_token = None
-    access_token_expires = datetime.datetime.now()
-    access_token_did_expire = True
-    client_id = None
-    client_secret = None
-    token_url = "https://accounts.spotify.com/api/token"
-    
-    def __init__(self, client_id, client_secret, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.client_id = client_id
-        self.client_secret = client_secret
+client_id2 = "c0bd529078254b8ba3e97dcc2be8328b"
+client_secret2 = "7f7fc4926ff74afeb4f6a325130517ad"
 
-    def get_client_credentials(self):
-        """
-        Returns a base64 encoded string
-        """
-        client_id = self.client_id
-        client_secret = self.client_secret
-        if client_secret == None or client_id == None:
-            raise Exception("You must set client_id and client_secret")
-        client_creds = f"{client_id}:{client_secret}"
-        client_creds_b64 = base64.b64encode(client_creds.encode())
-        return client_creds_b64.decode()
+# class SpotifyAPI(object):
+#     access_token = None
+#     access_token_expires = datetime.datetime.now()
+#     access_token_did_expire = True
+#     client_id = None
+#     client_secret = None
+#     token_url = "https://accounts.spotify.com/api/token"
     
-    def get_token_headers(self):
-        client_creds_b64 = self.get_client_credentials()
-        return {
-            "Authorization": f"Basic {client_creds_b64}"
-        }
+#     def __init__(self, client_id, client_secret, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.client_id = client_id
+#         self.client_secret = client_secret
+
+#     def get_client_credentials(self):
+#         """
+#         Returns a base64 encoded string
+#         """
+#         client_id = self.client_id
+#         client_secret = self.client_secret
+#         if client_secret == None or client_id == None:
+#             raise Exception("You must set client_id and client_secret")
+#         client_creds = f"{client_id}:{client_secret}"
+#         client_creds_b64 = base64.b64encode(client_creds.encode())
+#         return client_creds_b64.decode()
     
-    def get_token_data(self):
-        return {
-            "grant_type": "client_credentials"
-        } 
+#     def get_token_headers(self):
+#         client_creds_b64 = self.get_client_credentials()
+#         return {
+#             "Authorization": f"Basic {client_creds_b64}"
+#         }
     
-    def perform_auth(self):
-        token_url = self.token_url
-        token_data = self.get_token_data()
-        token_headers = self.get_token_headers()
-        r = requests.post(token_url, data=token_data, headers=token_headers)
-        if r.status_code not in range(200, 299):
-            return False
-        data = r.json()
-        now = datetime.datetime.now()
-        access_token = data['access_token']
-        expires_in = data['expires_in'] # seconds
-        expires = now + datetime.timedelta(seconds=expires_in)
-        self.access_token = access_token
-        self.access_token_expires = expires
-        self.access_token_did_expire = expires < now
-        return True
+#     def get_token_data(self):
+#         return {
+#             "grant_type": "client_credentials"
+#         } 
     
-    def get_access_token(self):
-        done_auth = self.perform_auth()
-        if not done_auth:
-            raise Exception("Auth Failed")
-        token = self.access_token
-        expires = self.access_token_expires
-        now = datetime.datetime.now()
-        if expires < now:
-            self.perform_auth()
-            return self.get_access_token()
-        elif token == None:
-            self.perform_auth()
-            return self.get_access_token()
-        return token
-    def search(self, query, search_type='track'):
+#     def perform_auth(self):
+#         token_url = self.token_url
+#         token_data = self.get_token_data()
+#         token_headers = self.get_token_headers()
+#         r = requests.post(token_url, data=token_data, headers=token_headers)
+#         if r.status_code not in range(200, 299):
+#             return False
+#         data = r.json()
+#         now = datetime.datetime.now()
+#         access_token = data['access_token']
+#         expires_in = data['expires_in'] # seconds
+#         expires = now + datetime.timedelta(seconds=expires_in)
+#         self.access_token = access_token
+#         self.access_token_expires = expires
+#         self.access_token_did_expire = expires < now
+#         return True
+    
+#     def get_access_token(self):
+#         done_auth = self.perform_auth()
+#         if not done_auth:
+#             raise Exception("Auth Failed")
+#         token = self.access_token
+#         expires = self.access_token_expires
+#         now = datetime.datetime.now()
+#         if expires < now:
+#             self.perform_auth()
+#             return self.get_access_token()
+#         elif token == None:
+#             self.perform_auth()
+#             return self.get_access_token()
+#         return token
+#     def search(self, query, search_type='track'):
         
-        access_token = self.get_access_token()
-        headers = {"Authorization" : f"Bearer {access_token}"}
-        endpoint = "https://api.spotify.com/v1/search?"
-        data = urlencode({"q" : query, "type" : search_type.lower()})#the query or item you rsearching for
-        lookup_url = f"{endpoint}{data}"
-        r = requests.get(lookup_url, headers=headers)
-        if r.status_code not in range(200,299):
-            return {'error'}
-        return r.json()
+#         access_token = self.get_access_token()
+#         headers = {"Authorization" : f"Bearer {access_token}"}
+#         endpoint = "https://api.spotify.com/v1/search?"
+#         data = urlencode({"q" : query, "type" : search_type.lower()})#the query or item you rsearching for
+#         lookup_url = f"{endpoint}{data}"
+#         r = requests.get(lookup_url, headers=headers)
+#         if r.status_code not in range(200,299):
+#             return {'error'}
+#         return r.json()
     
-    def artists(self, artist_id, search_type='artists'):
+#     def artists(self, artist_id, search_type='artists'):
         
-        access_token = self.get_access_token()
-        headers = {"Authorization" : f"Bearer {access_token}"}
-        endpoint = "https://api.spotify.com/v1/tracks?"
-        data = urlencode({"q" : artist_id, "type" : search_type.lower()})#the query or item you rsearching for
-        lookup_url = f"{endpoint}{data}"
-        r = requests.get(lookup_url, headers=headers)
-        if r.status_code not in range(200,299):
-            return {'error'}
-        return r.json()
+#         access_token = self.get_access_token()
+#         headers = {"Authorization" : f"Bearer {access_token}"}
+#         endpoint = "https://api.spotify.com/v1/tracks?"
+#         data = urlencode({"q" : artist_id, "type" : search_type.lower()})#the query or item you rsearching for
+#         lookup_url = f"{endpoint}{data}"
+#         r = requests.get(lookup_url, headers=headers)
+#         if r.status_code not in range(200,299):
+#             return {'error'}
+#         return r.json()
   
 
 
@@ -234,46 +241,61 @@ class SpotifyAPI(object):
 
 
 spotifyid = SpotifyAPI(client_id, client_secret)
+spotifyid2 = SpotifyAPI(client_id2, client_secret2)
 
-artist_list = []
-doneyet = 0
-for artist in clean_artists:  
+# artist_list = []
+# doneyet = 0
+# for artist in clean_artists:  
     
-    doneyet += 1
-    print('Grabbing artist: ', doneyet)
+#     doneyet += 1
+#     print('Grabbing artist: ', doneyet)
     
 artist_list = []   
+failed_index = {}
 i = 0 
 doneyet = 0   
 failcount = 0
-while i == doneyet:
+
+
+
+while i < len(clean_artists):
+    
     if spotifyid.get_artist(clean_artists[i]) != {}:
         failcount = 0
         artist_list.append(spotifyid.get_artist(clean_artists[i]))
         i += 1
         doneyet = i
         print('Grabbing artist: ', doneyet)
-    elif doneyet == len(clean_artists):
-        break
+    
     else:
         print('Failed Restarting last iteration')
         
         failcount += 1
-        if failcount == 15:
-            i += 1
-            doneyet = i
+        print('Fail Count: ', failcount)
+        if failcount > 15:
+            print('Trying second client')
+            if spotifyid2.get_artist(clean_artists[i]) != {}:
+                artist_list.append(spotifyid2.get_artist(clean_artists[i]))
+                failcount = 0
+            else:
+                print('Client2 failed, appending NONE')
+                artist_list.append('None')
+                failed_index[i] = clean_artists[i]
+                
+                i += 1
+                doneyet = i
+                failcount = 0
+
         continue
-    print('i: ', i)
-    print('doneyet: ', doneyet)
     
-for i in range(doneyet, len(clean_artists)):
-    if spotifyid.get_artist(clean_artists[i]) != {}:
-        artist_list.append(spotifyid.get_artist(clean_artists[i]))
-        doneyet = i
-        print('Grabbing artist: ', doneyet) 
-    else:
-        i -= 1
-    print(i)
+# for i in range(doneyet, len(clean_artists)):
+#     if spotifyid.get_artist(clean_artists[i]) != {}:
+#         artist_list.append(spotifyid.get_artist(clean_artists[i]))
+#         doneyet = i
+#         print('Grabbing artist: ', doneyet) 
+#     else:
+#         i -= 1
+#     print(i)
     
     
 genre_list = []
@@ -286,7 +308,6 @@ for genre in artist_list:
     doneyet2 += 1
     print('genre extracted: ', doneyet2)
        
-    
 save_artist = artist_list.copy()
     
 
